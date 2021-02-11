ddbb3de50a1f281a7fad20d69a21e102fdcac069
Vitalii Bairak <vitalii_b88@yahoo.com>
2021-02-11 02:02:17 +0200

    feat: added new option on app

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,10 @@ import { Command } from 'commander';
 const gendiff = new Command();
 
 gendiff
-  .version('0.0.1');
+  .version('0.0.1')
+
+gendiff
+  .option('-f, --format [type]', 'output format')
+  // .description('output format')
 
 gendiff.parse(process.argv);
