431a2f5ad7b3c8aeeae036231277378b3455ea3b
Vitalii Bairak <vitalii_b88@yahoo.com>
2021-02-20 20:35:19 +0200

    feat(add new function getdatafromjson()): add new function getdatafromjson()
    The function takes 2 arguments. Path to JSON file and format in which will return the result: string
    - will return the file structure in string format.object - return structure in object format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { getDataFromJSON } from '../src/functions.js';
 
 const gendiff = new Command();
 
 gendiff
-  .version('0.0.1')
+  .version('1.2.0');
 
 gendiff
-  .option('-f, --format [type]', 'output format')
-  // .description('output format')
+  .option('-f, --format [type]', 'output format');
 
 gendiff.parse(process.argv);
---
+++ src/functions.js
@@ -0,0 +1,31 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+/**
+ * The function takes 2 arguments.
+ * 1.path to JSON file
+ * 2.format in which will return the result:
+ * string - will return the file structure in string format.
+ * object - return structure in object format
+ */
+const getDataFromJSON = (packageDist, format) => {
+  const normalaizDist = resolve(packageDist);
+  const data = readFileSync(normalaizDist);
+
+  let res;
+  switch (format) {
+    case 'string':
+      res = JSON.stringify(data);
+      break;
+    case 'object':
+      res = JSON.parse(data);
+      break;
+    default:
+      res = data;
+  }
+
+  return res;
+};
+
+export {
+  getDataFromJSON,
+};
