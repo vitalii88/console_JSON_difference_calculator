c002f64669e47bc4115fa3b2954c92296d0505ec
Vitalii Bairak <vitalii_b88@yahoo.com>
2021-03-09 17:18:15 +0200

    test: add test from genFlatFileDiff fn

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -1,9 +1,15 @@
-import path from 'path';
-import fs from 'fs';
-import genFlatFileDiff from '../src/functions.js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { genFlatFileDiff } from '../src/functions.js';
 
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => resolve(cwd(), '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
+describe('genFlatFileDiff', () => {
+  test('to compare two flat JSON files', () => {
+    const result = genFlatFileDiff(getFixturePath('before.json'), getFixturePath('after.json')).trim();
+    const reference = readFile('expected_json').trim();
+    expect(reference).toBe(result);
+  });
+});
