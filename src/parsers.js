7d46280f64eea3bef2ff6a23c10fc43d6c554495
Vitalii Bairak <vitalii_b88@yahoo.com>
2021-03-27 23:14:17 +0200

    refactor: refactoring getUnqKeys()

--- src/parsers.js
+++ src/parsers.js
@@ -58,18 +58,72 @@ const genDiffArr = (keys, beforeArr, afterArr) => keys.reduce((acc, key) => {
 }, []);
 
 /**
+ * Retrieves unique keys
+ * @param firstObj
+ * @param secondObj
+ * @returns {string[]}
+ */
+const getUnqKeys = (firstObj, secondObj) => {
+  // return Object.getOwnPropertyNames(_.defaults(firstObj, secondObj));
+  return _.union(Object.keys(firstObj), Object.keys(secondObj));
+  // const key1 = Object.keys(firstObj);
+  // console.log('key1 ==>', key1);
+  // const key2 = Object.keys(secondObj);
+  // console.log('key2 ==>', key2);
+  // return _.union(key1, key2);
+}
+
+/**
+ *
+ */
+const builder = () => {
+
+}
+
+/**
  * The function takes two arguments: paths to files with flat objects.
  * The result of the function will be a line with marks about changes
  * in the second file in relation to the first.
+ * { key: xxx,
+ *   value: yyy,
+ *   status: areEqual, deleted, added
+ *    child: {}
+ * }
  */
 export default function genFlatFileDiff(file1, file2) {
   const format = extname(file1);
   const sortObject1 = getSortObjFromKey(getData(file1, format));
+  console.log('sortObject1 ==> ', sortObject1);
   const sortObject2 = getSortObjFromKey(getData(file2, format));
-  const objToArr1 = Object.entries(sortObject1);
-  const objToArr2 = Object.entries(sortObject2);
-  const allKeys = Object.getOwnPropertyNames(_.defaults(sortObject1, sortObject2));
+  console.log('sortObject2 ==> ', sortObject2);
+  // const objToArr1 = Object.entries(sortObject1);
+  // const objToArr2 = Object.entries(sortObject2);
 
-  const diffArr = genDiffArr(allKeys, objToArr1, objToArr2);
-  return `{\n${diffArr.join('\n')}\n}`;
+  const allKeys = getUnqKeys(sortObject1, sortObject2);
+  console.log('allKeys ==> ', allKeys);
+  console.log('sortObject1 After allKeys ==> ', sortObject1);
+  console.log('sortObject2 After allKeys ==> ', sortObject2);
+  const tree = allKeys.map((key) => {
+    console.log('Map Key => ', key);
+    console.log('sortObject1[key] => ', sortObject1[key]);
+    console.log('sortObject2[key] => ', sortObject2[key]);
+    // if (!_.has(sortObject1, key)) {
+    //   return {  };
+    // }
+  });
+
+
+  // const diffArr = genDiffArr(allKeys, objToArr1, objToArr2);
+  // return `{\n${diffArr.join('\n')}\n}`;
+  return 'END';
 }
+
+
+
+
+
+
+
+
+
+
